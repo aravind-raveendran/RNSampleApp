9afca8180341816d1e524b3a08c1b73c695e67a1
Aravind Raveendran <aravind.raveendran@dolby.com>
2024-10-03 17:12:20 +1000

    Add console logs to inspect the hang

--- App.tsx
+++ App.tsx
@@ -11,26 +11,28 @@ import Logger from './Logger.js';
 
 function App(): React.JSX.Element {
   useEffect(() => {
+    console.log(
+      'Logger init reads a DRM environment which hangs up the execution; check whether the log after the initialiser is printed on console',
+    );
     const logger = new Logger('App');
+    console.log('Logger init completed successfully and no hang has occured');
     logger.print();
   }, []);
 
+  // Check whether the text `Successfully loaded the app screen` is loaded on screen.
   return (
     <SafeAreaView>
-      <Text style={styles.baseText}>
-        Text is bold
-        <Text style={styles.innerText}> and red</Text>
+      <Text style={styles.innerText}>
+        Successfully loaded the app screen !!!
       </Text>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  baseText: {
-    fontWeight: 'bold',
-  },
   innerText: {
-    color: 'red',
+    fontWeight: 'bold',
+    color: 'green',
   },
 });
 
--- Logger.js
+++ Logger.js
@@ -3,9 +3,11 @@ import {rtcDrmEnvironments} from './drm/rtc-drm-transform.js';
 export default class Logger {
   constructor(name) {
     this.name = name;
-    console.log('Init Logger !!!');
+    console.log(
+      'Read the drm environment to see if it indefinitely blocks the execution???',
+    );
     const env = rtcDrmEnvironments.Production;
-    console.log('env !!!', env);
+    console.log('DRM environment is read successfully; ', env);
   }
 
   print() {
